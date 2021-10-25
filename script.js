8c320171d7a85cd28639f4afadcca8721525cdaf
vfejbe <92761064+vfejbe@users.noreply.github.com>
2021-10-25 11:55:49 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var a =[]
-function addMe() {
- var b=document.getElementById("food").value;
- document.getElementById("food").value="";
- a.push(b);
- }
-function listMe(){
- for(i=0; i<a.length;i++){
-   var newP = document.createElement('p');
-   var newText=document.createTextNode(a[i]);
-   newP.appendChild(newText);
-   document.body.appendChild(newP);
- }
-a=[] ;
+var a;
+
+functon getName() {
+  //get value from input field and save as "a"
+  a = document.getElementById('username').value;
+  //create LS key with value of "a"
+  localStorage.setItem('uName',a);
+  //automatically takes user to page two.html
+  location.href = "two.html";
+}
+
+function getUser() {
+  var b = localStorage.getItem('uName');
+  document.getElementById('placeholder').innerHTML = "hello" + b;
 }
